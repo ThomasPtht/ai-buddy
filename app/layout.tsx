460b862d722ce3321c13e75a46bf7f68f3dee451
Thomas Potherat <t.potherat@laposte.net>
2025-03-01 16:51:39 +0100

    feat: setup db with supabase and prisma, create category filter

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,19 +6,12 @@ import {
   UserButton
 } from '@clerk/nextjs'
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from '@/components/theme-provider';
+import { cn } from '@/lib/utils';
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const inter = Inter({ subsets: ["latin"]})
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -33,7 +26,7 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en" suppressHydrationWarning>
-        <body>
+        <body className={cn("bg-secondary", inter.className)}>
 
           <ThemeProvider
             attribute="class"
--- components/mobile-sidebar.tsx
+++ components/mobile-sidebar.tsx
@@ -2,6 +2,7 @@ import { Menu } from "lucide-react"
 import {
     Sheet,
     SheetContent,
+    SheetTitle,
     SheetTrigger
 } from '@/components/ui/sheet';
 import Sidebar from "@/components/sidebar"
@@ -14,6 +15,7 @@ const MobileSidebar = () => {
                     <Menu />
                 </SheetTrigger>
                 <SheetContent side="left" className="p-0 bg-secondary pt-10 w-32">
+                    <SheetTitle></SheetTitle>
                     <Sidebar />
                 </SheetContent>
             </Sheet>
